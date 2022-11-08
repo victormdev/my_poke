f7220c865012b0e5665e4a6cc7712bcc2dd5f2e3
Victor Macedo <victoralexandrino99@gmail.com>
2022-11-08 04:56:37 -0300

    create home, loading and error screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'features/home/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Container(),
+      home: HomePage(list: [],),
     );
   }
 }
